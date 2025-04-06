8a84d6731a1b2852bec27fc17ca88f393456273e
Quang Nguyen <105619694+quangnd2203@users.noreply.github.com>
2025-04-06 11:34:49 +0700

    Update BodyTrackingConfigurationBuilder.swift

--- ios/Classes/ConfigurationBuilders/BodyTrackingConfigurationBuilder.swift
+++ ios/Classes/ConfigurationBuilders/BodyTrackingConfigurationBuilder.swift
@@ -4,9 +4,9 @@ import Foundation
 @available(iOS 13.0, *)
 func createBodyTrackingConfiguration(_: [String: Any]) -> ARBodyTrackingConfiguration? {
     if ARBodyTrackingConfiguration.isSupported {
-        return ARBodyTrackingConfiguration(
-            automaticSkeletonScaleEstimationEnabled: true
-        )
+        let config = ARBodyTrackingConfiguration()
+        config.automaticSkeletonScaleEstimationEnabled = true
+        return config
     }
     return nil
 }
